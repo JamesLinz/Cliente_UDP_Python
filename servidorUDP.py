19c43a99c21540dce1de038a5865f549f29582c1
JamesLinz <jlalencar@gmail.com>
2021-11-15 14:06:35 -0300

    Criado um server

---
+++ servidorUDP.py
@@ -0,0 +1,19 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print("Socket criado com sucesso!")
+
+host = 'localhost'
+porta = 5433
+
+s.bind((host,porta))
+mensagem = '\nServidor: Olá cliente, tudo bem?'
+
+while 1:
+    dados, end = s.recvfrom(4096)
+
+    if dados:
+        print('Servidor enviando mensagem...')
+        s.sendto(dados + (mensagem.encode()), end)
+
